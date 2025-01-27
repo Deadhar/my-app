a2f6d3b89f35409dc97a874227651a35bc766725
Valeriy Moskalenko <moskalenkoderek@gmail.com>
2025-01-27 12:47:24 +0300

    Deploy to GitHub Pages

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './css/Button.css'
-
-const Button = ({ onClickHandler, value, className }) => {
-    return <button className={'btn ' + className} onClick={onClickHandler}>
-        {value}
-    </button>
-}
-export default Button
